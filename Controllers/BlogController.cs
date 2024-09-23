01727cbda80663129aba4136fd20355d3c39dd45
2eg4rizve <2eg4rizve@gmail.com>
2024-09-23 17:20:30 +0600

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using InternPipeline.Entities.RequestEntity;
+using InternPipeline.Managers.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternPipeline.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogManager _blogManager;
+
+        public BlogController(IBlogManager blogManager)
+        {
+            _blogManager = blogManager;
+        }
+
+        [HttpPost("create-blog")]
+        public async Task<IActionResult> CreateBlog(CreateBlogRequestEntity createBlogRequestEntity)
+        {
+            var response = await _blogManager.CreateBlog(createBlogRequestEntity);
+            return Ok(response);
+        }
+    }
+}
---
+++ Managers/Interface/IBlogManager.cs
@@ -0,0 +1,11 @@
+﻿
+using InternPipeline.Entities.RequestEntity;
+using InternPipeline.Entities.ResponseEntity;
+
+namespace InternPipeline.Managers.Interface
+{
+    public interface IBlogManager
+    {
+        Task<CommonResponse> CreateBlog(CreateBlogRequestEntity createBlogRequestEntity);
+    }
+}
